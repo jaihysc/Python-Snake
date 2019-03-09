a18419b24e8d31bbbe634e85cd3cec83848c759f
Johnny Cai <jaihysc@gmail.com>
2019-03-08 21:56:32 -0500

    Added movement that half works

--- snake.py
+++ snake.py
@@ -1,4 +1,8 @@
 import sys, termios, tty, os, time
+import select
+import asyncio, threading
+import termios
+
 print("Welcome to snake...inside...Python")
 
 #Game grid
@@ -10,6 +14,9 @@ height = 30
 #Snake
 snakeX = [1, 0]
 snakeY = [0, 0]
+headDirection = 'D'
+
+lastRanTime = 0
 
 #Main
 def drawGameGrid():
@@ -60,20 +67,55 @@ def MoveSnake(x, y):
         snakeX[0] = snakeX[0] + x
     if snakeY[0] + y >= 0 and snakeY[0] + y < height:
         snakeY[0] = snakeY[0] + y
-while True:
-    drawGameGrid()
-    
-    #Movement
-    char = getch()
 
-    if (char == "a"):
-        MoveSnake(-1, 0)
-    elif (char == "d"):
-        MoveSnake(1, 0)
-    elif (char == "w"):
-        MoveSnake(0, -1)
-    elif (char == "s"):
-        MoveSnake(0, 1)
-    elif (char == 't'):
+def MovementManager(lastRanTime):
+    #CHeck time since it was last ran, if over .1 seconds run
+
+    if time.time() > lastRanTime:
+        lastRanTime = time.time()
+        drawGameGrid()     
+        if (headDirection == 'L'):
+            MoveSnake(-1, 0)
+        elif (headDirection == 'R'):
+            MoveSnake(1, 0)
+        elif (headDirection == 'U'):
+            MoveSnake(0, -1)
+        elif (headDirection == 'D'):
+            MoveSnake(0, 1)
+        return lastRanTime
+
+#100% not stolen from stack overflow
+def isData():
+    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+def getKeyInput():
+    old_settings = termios.tcgetattr(sys.stdin)
+    try:
+        tty.setcbreak(sys.stdin.fileno())
+
+        if isData():
+            c = sys.stdin.read(1)
+            
+            return c
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+    
+
+while True:  
+    lastRanTime = MovementManager(lastRanTime)
+
+    #Get keyboard input
+    char = getKeyInput()
+
+    #Movement
+    if char == 'a':
+        headDirection = 'L'
+    elif char == 'd':
+        headDirection = 'R'
+    elif char == 'w':
+        headDirection = 'U'
+    elif char == 's':
+        headDirection = 'D'
+    elif char == 't':
         snakeX.append(snakeX[len(snakeX) - 1] - 1)
         snakeY.append(snakeY[len(snakeY) - 1] - 1)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,14 @@
-while True:
-    drawGameGrid()
+    # #Movement
+    # char = getch()
+
+    # if (char == "a"):
+    #     headDirection = 'L'
+    # elif (char == "d"):
+    #     headDirection = 'R'
+    # elif (char == "w"):
+    #     headDirection = 'U'
+    # elif (char == "s"):
+    #     headDirection = 'D'
+    # elif (char == 't'):
+    #     snakeX.append(snakeX[len(snakeX) - 1] - 1)
+    #     snakeY.append(snakeY[len(snakeY) - 1] - 1)
